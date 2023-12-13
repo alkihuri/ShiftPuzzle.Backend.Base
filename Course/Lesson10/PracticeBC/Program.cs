170fa7693cfcb96ba85bde13102fb935929b219b
alkihuri <alkihuri@gmail.com>
2023-12-14 00:50:44 +0100

    Материалы 10 го урока

---
+++ Course/Lesson10/PracticeBC/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        SetAge(age);
+    }
+
+
+    public void Introduce()
+    {
+        Console.WriteLine("Hello, my name is " + Name);
+    }
+
+
+    public void SetAge(int newAge)
+    {
+        if (newAge >= 0)
+        {
+            Age = newAge;
+        }
+        else
+        {
+            Console.WriteLine("Age cannot be negative.");
+        }
+    }
+}
+
+
+public class Employee : Person
+{
+    public string Position { get; set; }
+
+
+    public Employee(string name, int age, string position) : base(name, age)
+    {
+        Position = position;
+    }
+}
+
+
+public class PersonFileService
+{
+    
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        // List of people to write to and read from the file
+        var people = new List<Person>
+        {
+            new Person("Alice", 28),
+            new Person("Bob", 35),
+            new Employee("Charlie", 42, "Manager")
+        };
+
+
+        // Writing people to the file
+        //PersonFileService.WritePeopleToFile(people);
+
+
+        // Reading people from the file
+        //var peopleFromFile = PersonFileService.ReadPeopleFromFile();
+        
+        
+        foreach (var person in peopleFromFile)
+        {
+            person.Introduce();
+        }
+    }
+}
