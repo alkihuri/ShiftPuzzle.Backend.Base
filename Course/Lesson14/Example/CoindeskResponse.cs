f1c1d37c44ef3b8e6799728fa108d9c7a44c7d80
alkihuri <alkihuri@gmail.com>
2023-12-16 02:02:55 +0100

    Материалы 14-го урока

---
+++ Course/Lesson14/Example/CoindeskResponse.cs
@@ -0,0 +1,55 @@
+namespace Example
+{
+
+
+    public class CoindeskResponse // основной класс для десериализации
+    {
+        public Time time { get; set; }
+        public string disclaimer { get; set; }
+        public string chartName { get; set; }
+        public Bpi bpi { get; set; }
+    }
+
+     public class Bpi // ниже второстепенные классы, объекты которых вложенны в основной CoindeskResponse (а также друг в друга)
+    {
+        public USD USD { get; set; }
+        public GBP GBP { get; set; }
+        public EUR EUR { get; set; }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+
+    public class GBP
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+
+   
+
+    public class Time
+    {
+        public string updated { get; set; }
+        public DateTime updatedISO { get; set; }
+        public string updateduk { get; set; }
+    }
+
+    public class USD
+    {
+        public string code { get; set; }
+        public string symbol { get; set; }
+        public string rate { get; set; }
+        public string description { get; set; }
+        public double rate_float { get; set; }
+    }
+}
