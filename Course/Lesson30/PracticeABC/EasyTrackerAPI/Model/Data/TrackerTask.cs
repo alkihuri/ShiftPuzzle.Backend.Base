a6c978b776e9c39d2bd196dab2729d168a98cc86
alkihuri <alkihuri@gmail.com>
2024-03-10 22:09:44 +0100

    Матриалы 30 го урока

--- Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
+++ Course/Lesson30/PracticeABC/EasyTrackerAPI/Model/Data/TrackerTask.cs
@@ -1,4 +1,6 @@
 public class TrackerTask
 {  
-    public int ID { get; set; } 
+    public int ID { get; set; }
+    public string? Name { get; internal set; }
+    public string? Description { get; internal set; }
 }
