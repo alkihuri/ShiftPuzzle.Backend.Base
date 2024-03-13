57660ca9ea3e721b4c4320cc846100cbf0f45464
alkihuri <alkihuri@gmail.com>
2024-03-13 01:19:40 +0100

    Материалы 32-го урока

--- Course/Lesson32/PracticeABC/EasyTrackerAPI/Model/Data/User.cs
+++ Course/Lesson32/PracticeABC/EasyTrackerAPI/Model/Data/User.cs
@@ -15,8 +15,7 @@ public class User
     public long ID { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
-    public string? Password { get; set; } 
-    [JsonIgnore]
+    public string? Password { get; set; }  
     public List<TrackerTask>? Tasks { get; set; }  
   
 }
