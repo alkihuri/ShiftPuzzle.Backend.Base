04d6c2e763c8975151488928aba1b0b28edc4fe7
alkihuri <alkihuri@gmail.com>
2024-04-14 15:13:20 +0200

    Update AccountManager.cs

--- Course/Lesson37/PracticeABC/EasyTrackerAPI/Model/Implementation/AccountManager.cs
+++ Course/Lesson37/PracticeABC/EasyTrackerAPI/Model/Implementation/AccountManager.cs
@@ -21,26 +21,7 @@ public class AccountManager : IAccountManager
         {
             Console.WriteLine("Account with name " + account.Name + " already exists."); 
             return;
-        }
-
-        string [] domens = new string[3]{"ru","com","com"};
-
-
-        if(account.Name.Contains("@") 
-            && account.Name.Contains(".") 
-            && account.Name.Length > 5
-            && account.Name.Length < 50
-            && domens.Any(d => account.Name.Split('.').Last().Contains(d)))   
-        {    
-            account.Email = account.Name;  
-            account.Name = account.Name.Split('@').First();
-            Console.WriteLine($"Email of {account.Name} is valid."); 
-        }
-        else
-        {
-            account.Email = account.Name;   
-        }
-
+        } 
         account.ID = _context.Users.Count() + 1;    
         _context.Users.Add(account); 
         _context.SaveChanges();
@@ -61,12 +42,12 @@ public class AccountManager : IAccountManager
         if(_context.Users.Any(u => u.Name == account.Name && u.Password == account.Password))
         {
             CurrentUser = account;
-            Console.WriteLine("Account verified.");
+            Console.WriteLine("Account verified."); 
             return true;    
         }
         else 
         {
-            Console.WriteLine("Account not verified.");
+            Console.WriteLine("Account not verified."); 
             return false; 
         }    
     }
